6a28bf14d0b8e1969a810c35e3c46bb4aff882a8
Gustav Petersson <gustav.petersson222@gmail.com>
2026-04-06 12:46:58 +0200

    Gjorde om Measure så den fungerade med förra pushen

--- Measure.py
+++ Measure.py
@@ -1,11 +1,13 @@
 import time
 import numpy as np
+import os
 from Algoritmer.QuickSort import QuickSort
-from BubbleSort import BubbleSort
 
 def load_data(filename):
-    with open(filename, "r") as f:
-        return [int(line.strip()) for line in f]
+    filepath = os.path.join("Data", filename)
+    
+    with open(filepath, "r") as f:
+        return [int(line.strip()) for line in f if line.strip()]
 
 def run_python_quicksort(data):
 
@@ -20,17 +22,6 @@ def run_python_quicksort(data):
     return end - start
 
 
-def run_python_bubblesort(data):
-
-    arr = data.copy()
-
-    start = time.perf_counter_ns()
-
-    BubbleSort.bubbleSort(arr)
-
-    end = time.perf_counter_ns()
-
-    return end - start
 
 
 def run_numpy_mergesort(data):
@@ -58,9 +49,6 @@ def main():
 
         if algorithm == "quick":
             t = run_python_quicksort(data)
-
-        elif algorithm == "bubble":
-            t = run_python_bubblesort(data)
 
         elif algorithm == "numpy_merge":
             t = run_numpy_mergesort(data)
